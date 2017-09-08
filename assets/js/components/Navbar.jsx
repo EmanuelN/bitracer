1b5736a0ba3ab5ec104a7cb3e760b67b9f1d4af7
Saj <codersaj@gmail.com>
2017-09-08 17:14:22 +0000

    switching to react components

---
+++ assets/js/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+// Navbar component
+class Navbar extends Component {
+
+  render() {
+
+    return (
+
+
+       );
+
+  }
+
+}
+
+export default Navbar;
