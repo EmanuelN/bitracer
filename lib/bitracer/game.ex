44f26455174da536f04fdd29093fc3c17f57acb2
EmanuelN <emanuelneves10@gmail.com>
2017-09-11 16:21:29 -0400

    sending data for all horses

--- lib/bitracer/game.ex
+++ lib/bitracer/game.ex
@@ -11,7 +11,7 @@ defmodule Bitracer.Game do
     {:ok, json} = get_json()
     list = Enum.take_random(json, 5)
     [
-      a: Enum.at(list, 0), 
+      a: Enum.at(list, 0),
       b: Enum.at(list, 1),
       c: Enum.at(list, 2),
       d: Enum.at(list, 3),
@@ -52,6 +52,9 @@ defmodule Bitracer.Game do
   def horse_positions(horses) do
     horses = put_in(horses, [:a], updatehorse(horses[:a]))
     horses = put_in(horses, [:b], updatehorse(horses[:b]))
+    horses = put_in(horses, [:c], updatehorse(horses[:c]))
+    horses = put_in(horses, [:d], updatehorse(horses[:d]))
+    horses = put_in(horses, [:e], updatehorse(horses[:e]))
     horses
   end
 
@@ -85,12 +88,16 @@ defmodule Bitracer.Game do
   def handle_info(:work, state) do
     a = elem(Enum.at(state[:list], state[:pos]), 1)
     b = elem(Enum.at(state[:list], state[:pos]+1), 1)
-    BitracerWeb.Endpoint.broadcast! "chat:chat", "game_data", %{horse_a: a, horse_b: b}
+    c = elem(Enum.at(state[:list], state[:pos]+2), 1)
+    d = elem(Enum.at(state[:list], state[:pos]+3), 1)
+    e = elem(Enum.at(state[:list], state[:pos]+4), 1)
+    BitracerWeb.Endpoint.broadcast! "chat:chat", "game_data", %{horse_a: a, horse_b: b, horse_c: c, horse_d: d, horse_e: e}
     state = cond do
-      state[:pos] >= 1200 ->
+      state[:pos] >= 3000 ->
         %{state | :list => race_frames(horses_list(), []), :pos => 0}
       true ->
-        %{state | :pos => state[:pos] + 2}
+        %{state | :pos => state[:pos] + 5}
+        IO.puts inspect state[:list]
     end
 #TODO: remove this, for debugging purposes
 #IO.puts inspect state
@@ -99,6 +106,6 @@ defmodule Bitracer.Game do
   end
 
   defp schedule_work do
-    Process.send_after(self(), :work, 1000)
+    Process.send_after(self(), :work, 100)
   end
 end
